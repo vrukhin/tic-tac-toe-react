17b8ff3f0b1ed96b228681b9e1aefee8eaef7138
Pavel Vrukhin <vrukhin@protonmail.com>
2024-01-23 22:49:58 +0300

    Add handler for "O" moves

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,25 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
 
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+
+    if (squares[i]) {
+      return;
+    }
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
